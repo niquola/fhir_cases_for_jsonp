9f76d8c2cfa3587a6e51687018bec94406c94700
Danil Kutkevich <danil@kutkevich.org>
2014-04-29 21:00:34 +0400

    observation dummy applies substitution

--- observations/generate.sh
+++ observations/generate.sh
@@ -1,8 +1,12 @@
 num_rows=$1
 
 names=(
-    '"name": {"coding": [{"system": "http://loinc.org", "code": "8310-5", "display": "Body temperature"}], "text": "Body temperature"}'
-    '"name": {"coding": [{"system": "http://loinc.org", "code": "55284-4", "display": "Blood pressure systolic \& diastolic"}]}'
+    '"name": {"coding": [{"system": "http://loinc.org", "code": "8310-5", "display": "Body temperature"}], "text": "Body temperature"},'
+    '"name": {"coding": [{"system": "http://loinc.org", "code": "55284-4", "display": "Blood pressure systolic \& diastolic"}]},'
+)
+applies=(
+    '"appliesDateTime": "2014-01-29T09:30:10+01:00",'
+    '"appliesPeriod": {"start": "2013-04-02T09:30:10+01:00", "end": "2013-04-05T09:30:10+01:00"},'
 )
 statuses=(
     "amended"
@@ -21,11 +25,19 @@ for ((i = 1; i <= $num_rows; i++)); do
     if (($name_index < 2)); then
         name=${names[$name_index]}
     else
-        name="\"name\": {\"coding\": [{\"system\": \"http://loinc.org\", \"code\": \"noise-$name_index\", \"display\": \"noise $name_index\"}], \"text\": \"Noise $name_index\"}"
+        name="\"name\": {\"coding\": [{\"system\": \"http://loinc.org\", \"code\": \"noise-$name_index\", \"display\": \"noise $name_index\"}], \"text\": \"Noise $name_index\"},"
+    fi
+
+    apply_index=$((RANDOM%3))
+    if (($apply_index < 2)); then
+        apply=${applies[$apply_index]}
+    else
+        apply=""
     fi
 
     sed -e "s/{{patient_id}}/$patient_id/g"  \
         -e "s@{{name}}@$name@" \
+        -e "s@{{applies}}@$apply@" \
         -e "s/{{status}}/$status/g" \
         template.json
 done
