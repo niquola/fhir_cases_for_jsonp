67274ac7672cfec7cc481a09c0ddb6d597615c52
Danil Kutkevich <danil@kutkevich.org>
2014-04-29 19:52:13 +0400

    observations: more statuses

--- observations/generate.sh
+++ observations/generate.sh
@@ -1,9 +1,16 @@
 num_rows=$1
 
-statuses=("final")
+statuses=(
+    "amended"
+    "cancelled"
+    "entered in error"
+    "final"
+    "preliminary"
+    "registered"
+)
 
 for ((i = 1; i <= $num_rows; i++)); do
-    status=${statuses[$((RANDOM%1))]}
+    status=${statuses[$((RANDOM%6))]}
     patient_id=$(($i%1000))
     sed -e s/{{patient_id}}/$patient_id/  \
         -e "s/{{status}}/$status/g" \
