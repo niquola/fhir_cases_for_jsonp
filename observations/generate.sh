11aff9f61323408d6eb6c373074c60165ab3ba82
Danil Kutkevich <danil@kutkevich.org>
2014-04-30 16:19:57 +0400

    observations generation time reduced by 20%

--- observations/generate.sh
+++ observations/generate.sh
@@ -8,10 +8,6 @@ names=(
     '"name": {"coding": [{"system": "http://loinc.org", "code": "8310-5", "display": "Body temperature"}], "text": "Body temperature"},'
     '"name": {"coding": [{"system": "http://loinc.org", "code": "55284-4", "display": "Blood pressure systolic \& diastolic"}]},'
 )
-applies=(
-    '"appliesDateTime": "{{rand_date}}T09:30:10+01:00",'
-    '"appliesPeriod": {"start": "{{rand_date}}T09:30:10+01:00", "end": "{{rand_date2}}T09:30:10+01:00"},'
-)
 statuses=(
     "amended"
     "cancelled"
@@ -29,7 +25,7 @@ for ((i = 1; i <= $num_rows; i++)); do
     if (($name_index < 2)); then
         name=${names[$name_index]}
     else
-        name="\"name\": {\"coding\": [{\"system\": \"http://loinc.org\", \"code\": \"noise-$name_index\", \"display\": \"noise $name_index\"}], \"text\": \"Noise $name_index\"},"
+        name='"name": {"coding": [{"system": "http://loinc.org", "code": "noise-'"$name_index"'", "display": "noise '"$name_index"'"}], "text": "Noise '"$name_index"'"},'
     fi
 
     apply_index=$((RANDOM%3))
@@ -37,14 +33,23 @@ for ((i = 1; i <= $num_rows; i++)); do
         rand_y=$((RANDOM%2+2013))
         rand_m=$((RANDOM%11+1))
         rand_d=$((RANDOM%15+1))
-        rand_d2=$(($rand_d+3))
-        rand_date=`date -d "$rand_y-$rand_m-$rand_d" '+%Y-%m-%d'`
-        rand_date2=`date -d "$rand_y-$rand_m-$rand_d2" '+%Y-%m-%d'`
-        apply=$(
-            echo ${applies[$apply_index]} \
-                | sed -e "s/{{rand_date}}/$rand_date/" \
-                      -e "s/{{rand_date2}}/$rand_date2/"
-        )
+        rand_date=`date -d "$rand_y-$rand_m-$rand_d" '+%Y-%m'`
+        rand_date1="${rand_date}-"$rand_d
+        if (($apply_index == 0)); then
+            apply=$(
+                echo '"appliesDateTime": "{{rand_date}}T09:30:10+01:00",' \
+                    | sed -e "s/{{rand_date}}/$rand_date1/"
+            )
+        fi
+        if (($apply_index == 1)); then
+            rand_d2=$(($rand_d+3))
+            rand_date2="${rand_date}-"$rand_d2
+            apply=$(
+                echo '"appliesPeriod": {"start": "{{rand_date}}T09:30:10+01:00", "end": "{{rand_date2}}T09:30:10+01:00"},' \
+                    | sed -e "s/{{rand_date}}/$rand_date1/" \
+                          -e "s/{{rand_date2}}/$rand_date2/"
+            )
+        fi
     else
         apply=""
     fi
